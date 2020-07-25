1b988a3a0a49dca00227de1d954c934c49232a76
Ihor <igornepipenko@gmail.com>
2020-07-25 13:31:52 +0300

    Feature: observable base [ref:no-ref]

---
+++ 0-theory/src/old/motivation.ts
@@ -0,0 +1,24 @@
+//
+// const sequence = new Promise((res) => {
+//     let count = 1;
+//     setInterval(() => {
+//         res(count++);
+//     }, 1000)
+// })
+//
+// sequence.then((v: any) => terminalLog(v))
+// sequence.then((v: any) => terminalLog(v))
+// sequence.then((v: any) => terminalLog(v))
+
+// const sequence  = function* iteratorFn () {
+//   let item = 1;
+//   while (true){
+//       yield item++;
+//   }
+// }()
+//
+// terminalLog(sequence.next().value);
+// terminalLog(sequence.next().value);
+// terminalLog(sequence.next().value);
+// terminalLog(sequence.next().value);
+// terminalLog(sequence.next().value);
