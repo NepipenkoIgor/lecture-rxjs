abe51e1733d8eebea823868304b896b3e3508aff
Ihor <igornepipenko@gmail.com>
2020-07-25 13:59:22 +0300

    Feature: base create operators [ref:no-ref]

--- 0-theory/src/index.ts
+++ 0-theory/src/index.ts
@@ -1,39 +1,38 @@
 import '../../assets/css/style.css';
 import { terminalLog } from "../../utils/log-in-terminal";
-import { fromEvent, interval, Observable } from "rxjs";
-
-
-const  sequence = new Observable((subscriber)=>{
-    terminalLog('Init me')
-    let count = 1;
-    const intId = setInterval(() => {
-        count++;
-        subscriber.next(Date.now());
-        if(count === 5){
-            subscriber.complete();
-            clearInterval(intId);
-        }
-    }, 1000)
-})
-//     //interval(1000)
-sequence.subscribe((v) =>terminalLog(`Sub 1 => ${v}`),()=>{}, ()=>{
-    terminalLog('completed')
-})
-
-setTimeout(()=>{
-    sequence.subscribe((v) =>terminalLog(`Sub 2 => ${v}`),()=>{}, ()=>{
-        terminalLog('completed')
-    })
-} , 5000)
-
-// const sequence: Observable<MouseEvent> = new Observable((subscriber)=>{
-//     document.addEventListener('click', (e: MouseEvent)=>{
-//         subscriber.next(e)
-//     })
-// })
-
-// sequence.subscribe((v: MouseEvent) =>terminalLog(`Sub 1 => ${v.clientX}`))
+import { defer, from, iif, of, range, timer } from "rxjs";
+import { ajax, AjaxResponse } from "rxjs/ajax";
+import { map } from "rxjs/operators";
 //
-// setTimeout(()=>{
-//     sequence.subscribe((v: MouseEvent) =>terminalLog(`Sub 2 => ${v.clientX}`))
-// } , 5000)
+// of(1, 2, 3, 4, 5)
+//     .subscribe((v) => terminalLog(v))
+
+
+// from([1, 2, 3, 4, 5])
+//     .subscribe((v) => terminalLog(v))
+
+// range(2, 10)
+//     .subscribe((v) => terminalLog(v))
+
+// timer(5000, 1000)
+//     .subscribe((v) => terminalLog(v))
+// const random = Math.round(Math.random() * 10)
+// const sequence = iif(() => {
+//     return random >= 5;
+// }, of(`First, number is ${random}`), of(`Second, number is ${random}`))
+// const sequence = defer(() => {
+//     return random >= 5
+//         ? random >= 8
+//             ? of(`First, number is ${random}`)
+//             : of(`Third, number is ${random}`)
+//         : of(`Second, number is ${random}`)
+// })
+// sequence.subscribe((v) => terminalLog(v))
+
+// const sequence = from(fetch('http://learn.javascript.ru/courses/groups/api/participants?key=1c4jhut')
+//     .then((res)=>res.json()))
+// const sequence = ajax('http://learn.javascript.ru/courses/groups/api/participants?key=1c4jhut')
+//     .pipe(
+//         map((res: AjaxResponse) => res.response)
+//     )
+// sequence.subscribe((v) => console.log(v))
---
+++ 0-theory/src/old/observable-cold-hot.ts
@@ -0,0 +1,39 @@
+
+import { fromEvent, interval, Observable } from "rxjs";
+import { terminalLog } from "../../../utils/log-in-terminal";
+
+
+const  sequence = new Observable((subscriber)=>{
+    terminalLog('Init me')
+    let count = 1;
+    const intId = setInterval(() => {
+        count++;
+        subscriber.next(Date.now());
+        if(count === 5){
+            subscriber.complete();
+            clearInterval(intId);
+        }
+    }, 1000)
+})
+//     //interval(1000)
+sequence.subscribe((v) =>terminalLog(`Sub 1 => ${v}`),()=>{}, ()=>{
+    terminalLog('completed')
+})
+
+setTimeout(()=>{
+    sequence.subscribe((v) =>terminalLog(`Sub 2 => ${v}`),()=>{}, ()=>{
+        terminalLog('completed')
+    })
+} , 5000)
+
+// const sequence: Observable<MouseEvent> = new Observable((subscriber)=>{
+//     document.addEventListener('click', (e: MouseEvent)=>{
+//         subscriber.next(e)
+//     })
+// })
+
+// sequence.subscribe((v: MouseEvent) =>terminalLog(`Sub 1 => ${v.clientX}`))
+//
+// setTimeout(()=>{
+//     sequence.subscribe((v: MouseEvent) =>terminalLog(`Sub 2 => ${v.clientX}`))
+// } , 5000)
