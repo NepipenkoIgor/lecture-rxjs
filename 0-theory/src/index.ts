4d0eb0fdd7b8eff926a9e95231a1327b8a7730a9
Ihor <igornepipenko@gmail.com>
2020-07-25 14:20:18 +0300

    Feature: operators [ref:no-ref]

--- 0-theory/src/index.ts
+++ 0-theory/src/index.ts
@@ -1,38 +1,35 @@
 import '../../assets/css/style.css';
 import { terminalLog } from "../../utils/log-in-terminal";
-import { defer, from, iif, of, range, timer } from "rxjs";
-import { ajax, AjaxResponse } from "rxjs/ajax";
-import { map } from "rxjs/operators";
-//
-// of(1, 2, 3, 4, 5)
-//     .subscribe((v) => terminalLog(v))
+import { interval } from "rxjs";
+import { filter, map, skip, take, tap } from "rxjs/operators";
 
+const sequence1$ = interval(1000);
 
-// from([1, 2, 3, 4, 5])
-//     .subscribe((v) => terminalLog(v))
+/*
+  sequence1$: ---0---1---2---3---4---5---6--
+      filter((x)=>x%2 === 0)
+              ---0-------2-------4-------6--
+      tap((x)=>x*2)
+              ---0-------2-------4-------6--
+      map((x)=>x*2)
+              ---0-------4-------8-------12--
+      tap((x)=>x*2)
+              ---0-------4-------8-------12--
+      skip(2)
+              -------------------8-------12--
+      take(2)
+  sequence2$  -------------------8-------12|
+ */
 
-// range(2, 10)
-//     .subscribe((v) => terminalLog(v))
+const sequence2$ = sequence1$.pipe(
+    filter((x)=>x%2 === 0),
+    tap((x)=>x*2),
+    map((x) => x * 2),
+    tap((x)=>x*2),
+    skip(2),
+    take(2)
+)
 
-// timer(5000, 1000)
-//     .subscribe((v) => terminalLog(v))
-// const random = Math.round(Math.random() * 10)
-// const sequence = iif(() => {
-//     return random >= 5;
-// }, of(`First, number is ${random}`), of(`Second, number is ${random}`))
-// const sequence = defer(() => {
-//     return random >= 5
-//         ? random >= 8
-//             ? of(`First, number is ${random}`)
-//             : of(`Third, number is ${random}`)
-//         : of(`Second, number is ${random}`)
-// })
-// sequence.subscribe((v) => terminalLog(v))
-
-// const sequence = from(fetch('http://learn.javascript.ru/courses/groups/api/participants?key=1c4jhut')
-//     .then((res)=>res.json()))
-// const sequence = ajax('http://learn.javascript.ru/courses/groups/api/participants?key=1c4jhut')
-//     .pipe(
-//         map((res: AjaxResponse) => res.response)
-//     )
-// sequence.subscribe((v) => console.log(v))
+sequence2$.subscribe((v) => terminalLog(v), ()=>{}, ()=>{
+    terminalLog('completed')
+})
